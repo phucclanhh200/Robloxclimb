f3cea9d3652e7a9559e89fe1caed81e63e312405
phucclanhh200 <aimlanh59@gmail.com>
2025-06-03 18:58:48 +0700

    Climbandjumdvv.lua

--- Climbandjumdvv.lua
+++ Climbandjumdvv.lua
@@ -224,3 +224,10 @@ toggle.MouseButton1Click:Connect(function()
 		end
 	end
 end)
+
+local VirtualUser = game:service'VirtualUser'
+game:service'Players'.LocalPlayer.Idled:connect(function()
+	VirtualUser:Button2Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+	task.wait(1)
+	VirtualUser:Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+end)
